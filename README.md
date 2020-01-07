d63120b325a0edc11c1b3b53c9161cae7eb62a79
s3nh <steam.panek@gmail.com>
2020-01-07 19:15:50 +0100

    Fill .gitignore

---
+++ README.md
@@ -0,0 +1,1 @@
+BEngali kaggle competition 
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages 
+
+setup(author = 's3nh', 
+      packages = find_packages(),
+      name = 'bengaliai'
+      )
