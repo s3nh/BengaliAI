24d7b8ba709d9250cb73b82c6101f73cc2720c96
s3nh <steam.panek@gmail.com>
2020-01-12 14:00:57 +0100

    checker, just to move to desktop

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-from models.fishnet.loader import MoFishnet150
+import gc
+from models.fishnet.loader import MoFishnet150, load_checkpoint
+import pandas as pd 
+# FIshnet path from config? rethink
+from src.data_loader import _convert_data, get_and_split
+
+FISHNET_PATH = 'pretrain/fishnet/fishnet150_ckpt_welltrained.tar'
+gc.enable()
+def main():
+    mod = MoFishnet150(path=FISHNET_PATH)
+    mod.cuda()
+    train_data = pd.read_csv('data/train.csv') 
+    print(train_data.shape)
+    train_image = _convert_data(train_data = train_data)
+
+    print(train_image.shape)
+    x_train, x_test, y_train, y_test = get_and_split(train_image, label = 'grapheme_root')
+
+    x = x_train[0] 
+    x = x.cuda() 
+    x1, x2, x3 = mod(x)
+
+if __name__ == "__main__":
+    main()
+
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,5 +1,6 @@
 # Bengali AI data loader helper scripts. 
 from sklearn.model_selection import train_test_split
+import gc
 import pandas as pd
 import numpy as np 
 from PIL import Image
@@ -12,6 +13,7 @@ DATA_PATH = 'data/'
 FILES = os.listdir(DATA_PATH)
 FILES = [os.path.join(DATA_PATH, file) for file in FILES]
 assert len(FILES) > 0, 'theres no data'
+gc.enable();
 
 def resize(df, size=64, need_progress_bar=True):
     resized = {}
@@ -23,23 +25,22 @@ def resize(df, size=64, need_progress_bar=True):
 
 def _convert_data(datafile = 'data/train_image_data_0.parquet', train_data = pd.DataFrame()):
     assert os.path.exists(datafile)
-
     b_train_data = pd.merge(pd.read_parquet(datafile), 
                        train_data, on = 'image_id').drop(['image_id'], axis=1)
     train_image = b_train_data.drop(['grapheme_root', 'vowel_diacritic', 'consonant_diacritic','grapheme'], axis = 1)
+    del b_train_data
+    gc.collect()
     train_image = resize(train_image, size = 128)/255
     labels = ['grapheme_root', 'vowel_diacritic', 'consonant_diacritic']
     return train_image
 
-
-def get_and_split(df = pd.DataFrame(), label = 'grapheme_root')
+def get_and_split(df = pd.DataFrame(), label = 'grapheme_root'):
     assert df.shape[0] > 0, 'Data is not properly defined' 
     Y_train = b_train_data[label]    
+
     Y_train = pd.get_dummies(Y_train).values
-    x_train, x_test, y_train, y_test =  train_test_split(df, Y_train, test_size = 0.1, 
-    random_state = 4321)
-
-
+    x_train, x_test, y_train, y_test =  train_test_split(df, Y_train, test_size = 0.1, random_state = 4321)
+    return x_train, x_test, y_train, y_test
 
 def main():
     train_data = pd.read_csv('data/train.csv')
